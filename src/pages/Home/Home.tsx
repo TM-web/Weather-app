5ecbc6fa9a1f83e77186311afcdee30c2272c636
timurxp98 <timurxp98@gmail.com>
2022-02-01 12:32:13 +0500

    update

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-             <h1>ITS HOME
-             </h1>
+            <h1>ITS HOME</h1>
+
         </div>
     );
 };
